7a415e9f0716378e0afcb0162a8e840359521d1d
Sua12345 <sguzman-ortiz@albany.edu>
2026-09-04 13:32:29 -0400

    Miki Demo

--- Assets/NewMonoBehaviourScript.cs
+++ Assets/NewMonoBehaviourScript.cs
@@ -1,16 +1,13 @@
+using TMPro;
 using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public TextMeshProUGUI textbox;
+    private string tempString;
 
-    // Update is called once per frame
-    void Update()
+    public void ButtonClickMethod()
     {
-        
+        textbox.text = "I have changed";
     }
 }
